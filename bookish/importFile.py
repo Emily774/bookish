8f9f273d2d8fe3ac187589d4afa184f41d0382db
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-09-27 16:04:15 +0100

    connected python to sqlite and created contacts table with id, firstname, lastname and email

---
+++ bookish/importFile.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+try:
+    conn = sqlite3.connect('database.db')
+except Error as e:
+    print(e)
+
+# Create a cursor to allow to execute SQL commands
+cursor = conn.cursor()
+
+# Create a SQL Table
+sql_command = '''
+    CREATE TABLE IF NOT EXISTS contacts (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        Firstname TEXT, 
+        Lastname TEXT, 
+        Email TEXT
+    )'''
+
+cursor.execute(sql_command)
+
+# Commit the changes to the database
+conn.commit()
+
+conn.close()
