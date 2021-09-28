e12bf3e856123f0b7cf9e1e09c070379f13eea3d
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-09-28 14:05:47 +0100

    changes the table to contain book relevant values, title, author, etc.

--- bookish/importFile.py
+++ bookish/importFile.py
@@ -11,25 +11,56 @@ cursor = conn.cursor()
 
 # Create a SQL Table
 sql_command = '''
-    CREATE TABLE IF NOT EXISTS contacts (
+    CREATE TABLE IF NOT EXISTS bookish (
         Id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        Firstname TEXT, 
-        Lastname TEXT, 
-        Email TEXT
+        Title TEXT, 
+        Author TEXT, 
+        ISBN TEXT,
+        Copies INTEGER
     )'''
 
 cursor.execute(sql_command)
-
 # Commit the changes to the database
 conn.commit()
 
 sql_insert = '''
-    INSERT INTO contacts
-        (Firstname, Lastname, Email)
+    INSERT INTO bookish
+        (Title, Author, ISBN, Copies)
     VALUES (
-        'Lols', 'Chocolate', 'lols@chocolate.com'
+        'Do Androids Dream of Electric Sheep?', 'Isaac Asimov', 'AI2413000', 13
     )
-
 '''
+cursor.execute(sql_insert)
+conn.commit()
+
+select_data = 'SELECT * FROM bookish'
+cursor.execute(select_data)
+# row = cursor.fetchone()
+# print(row)
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+# manyrows = cursor.fetchmany(2)
+# for row in manyrows:
+#     print(row)
+
+# sql_view = '''SELECT * FROM contacts WHERE Firstname = '''
+# sql_update = '''
+#
+#
+# '''
+#
+# cursor.execute(sql_update)
+# conn.commit()
+#
+# sql_delete = '''
+#
+#
+# '''
+#
+# cursor.execute(sql_delete)
+# conn.commit()
 
 conn.close()
